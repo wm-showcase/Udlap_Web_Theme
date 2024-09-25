c0531eacff8fcafcb3f0061c06cd2678e88d80cd
Aftab Raja <aftab.raja@wavemaker.com>
2024-09-25 16:46:13 +0530

    Udlap web theme initial push

---
+++ Gruntfile.js
@@ -0,0 +1,132 @@
+/*global module,require*/
+const fs = require('fs');
+module.exports = function (grunt) {
+    'use strict';
+
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var wmBuildConfig = {
+        themes_src: 'src',
+        themes_tmp: 'tmp',
+        themes_dist: 'dist',
+        themes_fonts: 'components/bootstrap',
+        themes_icons: 'components/wavicon',
+        web_zip : 'web.zip'
+    };
+    // To generate compress config for themes
+    var buildCompressConfig = function () {
+        var compressConfig = {};
+           fs.readdirSync('./src/').forEach(function (file) {
+               if(fs.lstatSync('./src/'+file).isDirectory()){
+                   var obj = {
+                       options: {
+                           archive: '<%= config.themes_dist %>/' + file + '.zip',
+                           mode: 'zip'
+                       },
+                       files: [{
+                           src: ['**/*'],
+                           dot: true,
+                           cwd: '<%= config.themes_tmp %>/'+file+'/',
+                           expand: true
+                       }]
+                   };
+                   compressConfig[file] = obj;
+               }
+           });
+        return compressConfig;
+    };
+
+    grunt.initConfig({
+            config: wmBuildConfig,
+            clean : ['<%= config.themes_dist %>/', '<%= config.themes_tmp %>/'],
+            bower: {
+                install: {
+                    options: {
+                        targetDir: './components',
+                        layout: 'byComponent',
+                        install: true,
+                        verbose: false,
+                        cleanTargetDir: true,
+                        cleanBowerDir: false,
+                        copy: true
+                    }
+                }
+            },
+            xmlstoke: {
+                web: {
+                    options: {
+                        actions: [
+                            {type: 'R', xpath: '/properties/entry[@key="version"]', saveAs: 'web-version'},
+                            {type: 'R', xpath: '/properties/entry[@key="name"]', saveAs: 'web-name'}
+                        ]
+                    },
+                    files: [
+                        {'<%= config.themes_tmp %>/.wmproject.properties' : '<%= config.themes_tmp %>/web/.wmproject.properties'}
+                    ]
+                }
+            },
+            less: {
+                themes: {
+
+                    files: [
+                        {
+                            expand: true,
+                            cwd: '<%= config.themes_tmp %>/',
+                            src: ['**/style.less'],
+                            dest: '<%= config.themes_tmp %>/',
+                            ext: '.css'
+                        }
+                    ]
+                }
+            },
+            copy: {
+                themes: {
+                    files: [
+                        {
+                            cwd: '<%= config.themes_src %>/',
+                            src: '**',
+                            dot: true,
+                            expand: true,
+                            dest: '<%= config.themes_tmp %>/'
+                        }]
+                }
+            },
+            compress:buildCompressConfig()
+        }
+    );
+    grunt.registerTask("load-fonts", "copy fonts into themes", function () {
+        var copy, platform;
+        grunt.file.expand('src/**/fonts').forEach(function (dir) {
+            copy = grunt.config.get('copy') || {};
+            platform = dir.split('/')[2];
+            copy[dir] = {
+                files: [
+                    {
+                        cwd: wmBuildConfig.themes_fonts + '/fonts',
+                        dest: dir,
+                        expand: true,
+                        src: ['*']
+                    }
+                ]
+            };
+            if (platform === 'ios') {
+                copy[dir + '-wavicons'] = {
+                    files: [
+                        {
+                            cwd: wmBuildConfig.themes_icons + '/' + platform + '/fonts',
+                            dest: dir,
+                            expand: true,
+                            src: ['*']
+                        }
+                    ]
+                };
+            }
+            grunt.config.set('copy', copy);
+        });
+        grunt.task.run('copy');
+    });
+    grunt.registerTask('themes', ['clean', 'bower', 'copy', 'load-fonts', 'less:themes', 'xmlstoke', 'compress']);
+};
+
--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# Udlap_Web_Theme
+INSTRUCTIONS
+============
+
+Install Prerequisites
+--------
+
+* Node.js v10.17.x+
+
+You can check if you have Node and npm installed by typing:
+<pre>
+$ node --version && npm --version
+</pre>
+
+If you need to upgrade or install Node, the easiest way is to use an installer for your platform. Download the .msi for Windows or .pkg for Mac from the [NodeJS website](https://nodejs.org/download/).
+
+* Git
+You can check if you have Git installed by typing:
+<pre>
+$ git --version
+</pre>
+If you don't have Git, grab the installers from the [git website](http://git-scm.com/).
+
+
+Install Grunt & Bower
+--------
+
+Once you’ve got Node installed, install the Grunt and Bower:
+<pre>
+$ npm install --g bower grunt-cli
+</pre>
+
+(Make sure above commands execute with sudo/administrator permissions depending on OS eg, UNIX)
+
+Setup WaveMaker Theme Repository
+--------
+To clone the Udlap_Web_Theme repository, use git clone:
+
+<pre>
+ git clone https://github.com/wavemaker/Udlap_Web_Theme.git
+ cd Udlap_Web_Theme
+ npm install
+</pre>
+
+Understanding the Repository
+--------
+<pre>
++--src
+|  +--web
+|     +--style.less
+|     +--variable.less
+|     +--.wmproject.properties
+|     +--theme.png
+|     +--fonts/
++--dist
+|  +--web.zip
++--Gruntfile.js
+|
++--package.json
+|
++--bower.json
+</pre>
+
+Fonts
+-----
+There will be references to Roboto regular fonts in the variables.less(src/web), to avail them please download Roboto regular fonts and paste them in fonts directory(src/web/fonts).
+
+Build
+--------
+To build the WaveMaker Theme run the below command from Udlap_Web_Theme directory
+
+<pre>
+cd Udlap_Web_Theme
+grunt themes
+</pre>
+
